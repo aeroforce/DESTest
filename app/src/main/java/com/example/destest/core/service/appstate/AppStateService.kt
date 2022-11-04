ffea2d893c0d4a0a60a585dc5020bdd0ea6d3048
Jacek Dziewierski <jacek.dziewierski@intive.com>
2022-11-04 07:49:23 +0100

    Kdocs

--- app/src/main/java/com/example/destest/core/service/appstate/AppStateService.kt
+++ app/src/main/java/com/example/destest/core/service/appstate/AppStateService.kt
@@ -2,13 +2,29 @@ package com.example.destest.core.service.appstate
 
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Responsible for Application state functionality
+ */
+
 interface AppStateService {
 
+    /**
+     * @return busy state of app used for handling common loading overlay visibility
+     */
     fun isBusy(): Flow<Boolean>
 
+    /**
+     * Sets busy state of app used for loading overlay visibility
+     */
     fun setBusy(isBusy: Boolean)
 
+    /**
+     * @return connectivity problem state, used to show offline informational label
+     */
     fun isConnectionProblem(): Flow<Boolean>
 
+    /**
+     * Sets connection problem state
+     */
     fun setConnectionProblem(isProblem: Boolean)
 }
